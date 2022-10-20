1597985efdfb05470502a1c47a7fd762ac47c235
travelSnail0719 <111340279+travelSnail0719@users.noreply.github.com>
2022-10-20 21:21:37 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 3.67 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -0,0 +1,14 @@
+function solution(s) {
+    var answer = [];
+    
+    let stringToArray = s.split(' ');
+    console.log(stringToArray);
+    
+    stringToArray.forEach((item, idx) => {
+        let first = stringToArray[idx].charAt(0).toUpperCase();
+        let others = stringToArray[idx].slice(1).toLowerCase();
+        answer.push(first + others);   
+    })
+    
+    return answer.join(' ');
+}
