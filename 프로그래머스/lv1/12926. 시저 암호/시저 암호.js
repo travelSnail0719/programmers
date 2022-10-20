ecc0b9d47ce93025ce42e92771be5013660f4dc8
travelSnail0719 <111340279+travelSnail0719@users.noreply.github.com>
2022-10-20 21:33:51 +0900

    [level 1] Title: 시저 암호, Time: 4.54 ms, Memory: 37.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.js
@@ -0,0 +1,36 @@
+function solution(s, n) {
+    let result = [];
+    let answer = s.split('')
+    let stringToAscii = [];
+    let exception;
+    
+    for(let i = 0; i < answer.length; i++){
+        stringToAscii.push(answer[i].charCodeAt())
+    }
+    
+    for(let i = 0; i < stringToAscii.length; i++){
+        // 대문자일 경우
+        if(stringToAscii[i] >= 65 && stringToAscii[i] <= 90){
+            exception = stringToAscii[i] + n;
+            if(exception > 90){
+                exception = 64 + (exception - 90);
+            }
+        // 소문자일 경우
+        } else if(stringToAscii[i] >= 97 && stringToAscii[i] <= 122){
+            exception = stringToAscii[i] + n;
+            if(exception > 122){
+                exception = 96 + (exception - 122);
+            }
+        // 공백
+        } 
+        else {
+            exception = stringToAscii[i] + n; 
+            if(stringToAscii[i] == 32){
+                exception = stringToAscii[i];
+            }
+        }
+        result.push(String.fromCharCode(exception))
+    }
+    return result.join('');
+
+}
