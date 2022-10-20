4ecf82909b37beb0b9f0349540e7074762c8b76b
travelSnail0719 <ycbkr0719@gmail.com>
2022-10-20 21:15:39 +0900

    delete folder

--- Level1/2016.js
+++
@@ -1,4 +0,0 @@
-function solution(a, b) {
-    var answer = new Date(`2016-${a}-${b}`).toString().substr(0,3).toUpperCase();
-    return answer;
-}
--- Level1/stringDesc.js
+++
@@ -1,5 +0,0 @@
-function solution(s) {
-    let answer = '';
-    answer = s.split('').sort((a, b) => a < b ? 1 : -1).join("")
-    return answer;
-}
