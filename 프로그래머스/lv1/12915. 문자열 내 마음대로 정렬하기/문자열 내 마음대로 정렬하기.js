55eb3084b332a39a8c181a2d5903ef13ca76c879
travelSnail0719 <111340279+travelSnail0719@users.noreply.github.com>
2022-10-27 22:52:10 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -0,0 +1,16 @@
+function solution(strings, n) {
+    let answer = [];
+    
+    strings.sort((a, b) =>{
+        if(a[n] < b[n]){
+            return -1;
+        } else if(a[n] > b[n]){
+            return 1;
+        } else if(a < b){
+            return -1;
+        } else if(a > b){
+            return 1;
+        }
+    })
+    return strings;
+}
