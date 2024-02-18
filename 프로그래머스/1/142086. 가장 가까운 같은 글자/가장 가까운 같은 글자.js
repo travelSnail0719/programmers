47e19f7eef33d92b27a809667a96b38407a03981
travelSnail0719 <111340279+travelSnail0719@users.noreply.github.com>
2024-02-19 00:35:49 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 3.30 ms, Memory: 36.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -0,0 +1,14 @@
+function solution(s) {
+    s = s.split('');
+    
+    const answer = s.map((item, idx) => {
+        let lastIdx = s.lastIndexOf(item, idx - 1);
+        
+        if(idx === 0){
+            lastIdx = -1;
+        } 
+        
+        return lastIdx !== -1 ? idx - lastIdx : -1;
+    })
+    return answer;
+}
